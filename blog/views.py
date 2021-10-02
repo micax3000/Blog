f6a980e98d3c1116d9522af064a68fa9b5366f98
micax <milan.veljkovic0097@gmail.com>
2021-10-03 01:08:27 +0200

    added home page, about page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Micax',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 1, 2021'
+    },
+    {
+        'author': 'Maja',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 1, 2021'
+    }
+
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    contex = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', contex)
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html',{'title': 'About'})
 # Create your views here.
+def login(request):
+    return render(request, 'blog/login.html',{'title': 'Login'})
